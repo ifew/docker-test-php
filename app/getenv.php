dc000db0ac2dd5ab50db5fb5a32293124f07d6d5
Chitpong Wuttanan <chitpong@orisma.com>
2021-09-14 19:29:27 +0700

    add new env var

--- app/getenv.php
+++ app/getenv.php
@@ -4,10 +4,14 @@ $username = getenv('MYSQL_USER');
 $password = getenv('MYSQL_PASSWORD');
 $dbname = getenv('MYSQL_DATABASE');
 $test = getenv('TEST');
+$secret_username = getenv('SECRET_USERNAME');
+$secret_password = getenv('SECRET_PASSWORD');
 
 echo 'MYSQL_DB_HOST: '.$hostname.'<br>';
 echo 'MYSQL_USER: '.$username.'<br>';
 echo 'MYSQL_PASSWORD: '.$password.'<br>';
 echo 'MYSQL_DATABASE: '.$dbname.'<br>';
 echo 'TEST: '.$test.'<br>';
+echo 'SECRET_USERNAME: '.$secret_username.'<br>';
+echo 'SECRET_PASSWORD: '.$secret_password.'<br>';
 ?>
